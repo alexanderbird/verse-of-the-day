12fc6d86935700606b8b767d06868af2c0c48228
Alexander Bird <alex@alexanderbird.software>
2021-01-03 19:41:56 -0800

    Configure as a minimal one page jekyll site

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function renderOneDay() {
+  readonly local path=$0
+  
+  readonly local title=$(echo "$path" | sed "s/.\/\([0-9]\{4\}\)\/\([0-9]\{2\}\)\/\([0-9]\{2\}\)\.md/## \1-\2-\3/")
+  echo
+  echo $title
+  echo
+  cat $path
+}
+
+export -f renderOneDay
+
+echo ---
+echo ---
+cat ./README.md
+find . -regex "./[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\.md" | sort -r | xargs -n 1 bash -c renderOneDay
---
+++ netlify-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./build.sh > index.md
+
+bundle exec jekyll build

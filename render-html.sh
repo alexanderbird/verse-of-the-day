e20c8977cdd5234bbfeafce960a121c50331e5d6
Alexander Bird <alex@alexanderbird.software>
2021-01-03 23:17:46 -0800

    Add PWA manifest.json

--- render-html.sh
+++ render-html.sh
@@ -14,6 +14,7 @@ cat >> ./www/index.html << EOF
     <title>Verse of the Day</title>
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📖</text></svg>">
     <meta name="description" content="Alex's verse of the day">
+    <link rel="manifest" href="manifest.json">
 
     <!-- https://watercss.kognise.dev/ -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
